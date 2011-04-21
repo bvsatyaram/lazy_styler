f900fc5bff68106e1acd15fd530e2a09786856cb
B V Satyaram <bvsatyaram@gmail.com>
2011-04-21 22:54:20 +0530

    Round implementation

--- lazy_styler.js
+++ lazy_styler.js
@@ -1,3 +1,21 @@
 $(document).ready(function(){
-  $(".lazy_style").addClass("test");
+  $('.lazyStyle').each(function(eleIndex,styleElement) {
+    elementClasses = ($(styleElement).attr('class').split(' '))
+    $.each(elementClasses, function (index, eleValue) {
+      var words = eleValue.split('_')
+      if (words[0] == 'lazy')
+      {
+        if (words[1] == 'round')
+        {
+          handleRoundEdges(styleElement, words)
+        }
+      }
+    });
+  })
 });
+
+
+function handleRoundEdges(styleElement, words)
+{
+  $(styleElement).css('-moz-border-radius', words[2]+'px')
+}
