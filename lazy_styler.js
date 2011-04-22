ce46d84c7b316981eef76b00e9ee2efe21ba5098
B V Satyaram <bvsatyaram@gmail.com>
2011-04-22 15:50:15 +0530

    HAndling generic styles

--- lazy_styler.js
+++ lazy_styler.js
@@ -1,21 +1,70 @@
 $(document).ready(function(){
   $('.lazyStyle').each(function(eleIndex,styleElement) {
     elementClasses = ($(styleElement).attr('class').split(' '))
-    $.each(elementClasses, function (index, eleValue) {
-      var words = eleValue.split('_')
-      if (words[0] == 'lazy')
-      {
-        if (words[1] == 'round')
-        {
-          handleRoundEdges(styleElement, words)
+    $.each(elementClasses, function (classIndex, eleValue) {
+      var styleArgs = eleValue.split('_')
+      if (styleArgs[0] == 'lazy') {
+        handleLazyStyling(styleElement, styleArgs)
+      }
+
+      if (styleArgs[0] == 'lazy') {
+        if (styleArgs[1] == 'round') {
+          handleRoundEdges(styleElement, styleArgs)
         }
       }
     });
   })
 });
 
+function handleLazyStyling(styleElement, styleArgs) {
+  if (styleArgs[1] == 'round') {
+    handleRoundEdges(styleElement, styleArgs);
+  }
+  else if (styleArgs[1] == 'shadow') {
+    handleBoxShadow(styleElement, styleArgs);
+  }
+  else {
+    handleGenericStyle(styleElement, styleArgs);
+  }
+}
 
-function handleRoundEdges(styleElement, words)
-{
-  $(styleElement).css('-moz-border-radius', words[2]+'px')
+function handleRoundEdges(styleElement, styleArgs) {
+  styleParams = fetchStyleParams(styleArgs)
+  $(styleElement).css({
+    '-moz-border-radius': styleParams,
+    '-webkit-border-radius': styleParams,
+    'border-radius': styleParams
+  })
+}
+
+function handleBoxShadow(styleElement, styleArgs) {
+  styleParams = fetchStyleParams(styleArgs)
+  $(styleElement).css({
+    '-moz-box-shadow': styleParams,
+    '-webkit-box-shadow': styleParams,
+    'box-shadow': styleParams
+  })
+}
+
+function handleGenericStyle(styleElement, styleArgs) {
+  styleProp = styleArgs[1]
+  styleParams = fetchStyleParams(styleArgs)
+  $(styleElement).css(styleProp, styleParams)
+}
+
+function fetchStyleParams(styleArgs, padToFour) {
+  styleParams = '';
+  $.each(styleArgs, function(styleIndex, styleValue) {
+    if (styleIndex > 1) {
+      styleParams += styleValue;
+      if (styleValue == parseInt(styleValue).toString()) {
+        styleParams += 'px'
+      }
+      if (styleIndex != (styleArgs.length - 1)) {
+        styleParams += ' '
+      }
+    }
+  })
+
+  return styleParams;
 }
