b2675aa4637a9c4d03384db0aa3a20accd106bb3
B V Satyaram <bvsatyaram@gmail.com>
2011-04-22 16:44:25 +0530

    Converting to plugin

--- lazy_styler.js
+++ lazy_styler.js
@@ -1,70 +1,81 @@
-$(document).ready(function(){
-  $('.lazyStyle').each(function(eleIndex,styleElement) {
-    elementClasses = ($(styleElement).attr('class').split(' '))
-    $.each(elementClasses, function (classIndex, eleValue) {
-      var styleArgs = eleValue.split('_')
-      if (styleArgs[0] == 'lazy') {
-        handleLazyStyling(styleElement, styleArgs)
-      }
-
-      if (styleArgs[0] == 'lazy') {
-        if (styleArgs[1] == 'round') {
-          handleRoundEdges(styleElement, styleArgs)
+(function( $ ){
+  $.fn.lazyStyler = function() {
+    
+    this.each(function(eleIndex,styleElement) {
+      elementClasses = ($(styleElement).attr('class').split(' '));
+      $.each(elementClasses, function (classIndex, eleValue) {
+        var styleArgs = eleValue.split('_');
+        if (styleArgs[0] == 'lazy') {
+          handleLazyStyling(styleElement, styleArgs);
         }
-      }
+
+        if (styleArgs[0] == 'lazy') {
+          if (styleArgs[1] == 'round') {
+            handleRoundEdges(styleElement, styleArgs);
+          }
+        }
+      });
     });
-  })
-});
 
-function handleLazyStyling(styleElement, styleArgs) {
-  if (styleArgs[1] == 'round') {
-    handleRoundEdges(styleElement, styleArgs);
-  }
-  else if (styleArgs[1] == 'shadow') {
-    handleBoxShadow(styleElement, styleArgs);
-  }
-  else {
-    handleGenericStyle(styleElement, styleArgs);
-  }
-}
-
-function handleRoundEdges(styleElement, styleArgs) {
-  styleParams = fetchStyleParams(styleArgs)
-  $(styleElement).css({
-    '-moz-border-radius': styleParams,
-    '-webkit-border-radius': styleParams,
-    'border-radius': styleParams
-  })
-}
-
-function handleBoxShadow(styleElement, styleArgs) {
-  styleParams = fetchStyleParams(styleArgs)
-  $(styleElement).css({
-    '-moz-box-shadow': styleParams,
-    '-webkit-box-shadow': styleParams,
-    'box-shadow': styleParams
-  })
-}
-
-function handleGenericStyle(styleElement, styleArgs) {
-  styleProp = styleArgs[1]
-  styleParams = fetchStyleParams(styleArgs)
-  $(styleElement).css(styleProp, styleParams)
-}
-
-function fetchStyleParams(styleArgs, padToFour) {
-  styleParams = '';
-  $.each(styleArgs, function(styleIndex, styleValue) {
-    if (styleIndex > 1) {
-      styleParams += styleValue;
-      if (styleValue == parseInt(styleValue).toString()) {
-        styleParams += 'px'
+    function handleLazyStyling(styleElement, styleArgs) {
+      if (styleArgs[1] == 'round') {
+        handleRoundEdges(styleElement, styleArgs);
       }
-      if (styleIndex != (styleArgs.length - 1)) {
-        styleParams += ' '
+      else if (styleArgs[1] == 'shadow') {
+        handleBoxShadow(styleElement, styleArgs);
+      }
+      else {
+        handleGenericStyle(styleElement, styleArgs);
       }
     }
-  })
 
-  return styleParams;
-}
+    function handleRoundEdges(styleElement, styleArgs) {
+      styleParams = fetchStyleParams(styleArgs);
+      $(styleElement).css({
+        '-moz-border-radius': styleParams,
+        '-webkit-border-radius': styleParams,
+        'border-radius': styleParams
+      });
+    }
+
+    function handleBoxShadow(styleElement, styleArgs) {
+      styleParams = fetchStyleParams(styleArgs);
+      $(styleElement).css({
+        '-moz-box-shadow': styleParams,
+        '-webkit-box-shadow': styleParams,
+        'box-shadow': styleParams
+      });
+    }
+
+    function handleGenericStyle(styleElement, styleArgs) {
+      styleProp = styleArgs[1];
+      styleParams = fetchStyleParams(styleArgs);
+      $(styleElement).css(styleProp, styleParams);
+    }
+
+    function fetchStyleParams(styleArgs, padToFour) {
+      styleParams = '';
+      $.each(styleArgs, function(styleIndex, styleValue) {
+        if (styleIndex > 1) {
+          styleParams += styleValue;
+          if (styleValue == parseInt(styleValue).toString()) {
+            styleParams += 'px';
+          }
+          if (styleIndex != (styleArgs.length - 1)) {
+            styleParams += ' ';
+          }
+        }
+      })
+
+      return styleParams;
+    }
+
+  };
+  
+})( jQuery );
+
+
+// Invoke the plugin
+$(document).ready(function(){
+  jQuery(".lazyStyle").lazyStyler();
+})
